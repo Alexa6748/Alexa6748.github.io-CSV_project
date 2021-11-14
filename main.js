4841236cc9c8d8518d76d02e9de1b04f122e2e3b
Alexa6748 <sandjieva.alex@gmail.com>
2021-11-14 12:54:59 +0300

    4

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
